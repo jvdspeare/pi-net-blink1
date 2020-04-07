c7b8ea08dbbc6797dd28782e16d7a9c2c7f8caf0
jvdspeare <jvdspeare@gmail.com>
2020-04-07 10:09:15 +0000

    test

--- pi-net-blink1.py
+++ pi-net-blink1.py
@@ -3,11 +3,12 @@ import time
 import os
 from blink1.blink1 import Blink1
 
-
 hosts = {
     'edge router': ['192.168.20.1', 'red'],
-    'secondary DNS': ['192.168.20.244', 'blue']
+    'secondary DNS': ['192.168.20.5', 'blue']
 }
+
+status = list()
 
 
 def blink(colour):
@@ -22,13 +23,20 @@ def ping(host):
     return response
 
 
-for x, y in hosts.items():
-    print(x, y)
-    if ping(y[0]) == 0:
-        print('up')
+while True:
+    for x, y in hosts.items():
+        print(x, y)
+        if ping(y[0]) == 0:
+            status.append(1)
+            print(x + 'up')
+        else:
+            status.append(0)
+            print(x + 'down')
+            blink(y[1])
+
+    if any(status) == 0:
+        time.sleep(5)
+    else:
         b2 = Blink1()
         b2.off()
         b2.close()
-    else:
-        print('down')
-        blink(y[1])
