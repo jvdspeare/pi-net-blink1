569f087d826921f4778851090d06ed3436fa0dbb
jvdspeare <jvdspeare@gmail.com>
2020-04-07 08:13:47 +0000

    test

--- pi-net-blink1.py
+++ pi-net-blink1.py
@@ -1,6 +1,22 @@
 # import
 import time
+import os
 from blink1.blink1 import Blink1
 
-b1 = Blink1()
-b1.fade_to_color(100, 'red')
+
+def blink(colour):
+    b1 = Blink1()
+    b1.fade_to_color(100, colour)
+
+
+def ping(host):
+    response = os.system('ping -c 1 ' + host)
+    return response
+
+
+if ping('192.168.20.1') == 0:
+    print('up')
+    blink('blue')
+else:
+    print('down')
+    blink('yellow')
