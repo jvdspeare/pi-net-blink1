4b81f419607b02648c40120b210f95500f0155ba
jvdspeare <jvdspeare@gmail.com>
2020-04-14 08:55:33 +0000

    jvds

--- pi-net-blink1.py
+++ pi-net-blink1.py
@@ -3,6 +3,7 @@ import time
 import os
 from blink1.blink1 import Blink1
 
+# define the hosts to monitor (name, ip/hostname, colour for blink)
 hosts = {
     'google DNS': ['8.8.8.8', 'Red'],
     'telstra gateway max': ['192.168.20.1', 'Orange'],
@@ -13,39 +14,47 @@ hosts = {
     'st2': ['192.168.200.65', 'Red']
 }
 
+# list to track status
 status = list()
 
+# define sleep time for loop, this affects the frequency at which hosts are monitored (adapted to the number of hosts)
+sleep_time = len(hosts) * 3
 
+
+# blink the blink
 def blink(colour):
     b1 = Blink1()
     b1.play_pattern(colour)
     b1.close()
 
 
-def ping():
+# ping the hosts
+def ping(down_count=0):
     for x in hosts.values():
         response = os.system('ping -c 1 ' + x[0])
         if response == 0:
             status.append(1)
         else:
             status.append(0)
+            down_count += 1
+    return down_count
 
 
+# ping hosts, blink the blink, wait before testing again
 while True:
-    ping()
-    blink_colours = '10'
+    blink_colours = sleep_time / ping()
 
     for idx, item in enumerate(hosts.items()):
         if status[idx] == 0:
-            blink_colours = blink_colours + ', ' + item[1][1] + ',0.5,0, Black,0.5,0'
+            blink_colours = str(blink_colours) + ', ' + item[1][1] + ',0.5,0, Black,0.5,0'
 
     if 0 in status:
         blink(blink_colours)
-        time.sleep(30)
+        time.sleep(sleep_time)
     else:
         b2 = Blink1()
         b2.off()
         b2.close()
-        time.sleep(30)
+        time.sleep(sleep_time)
 
     status.clear()
